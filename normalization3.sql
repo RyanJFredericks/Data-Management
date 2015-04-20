9af6f208fba8ab6c17172f3f939d1facb4b52e58
RyanJFredericks <ryan.fredericks1@marist.edu>
2015-04-20 11:31:11 -0400

    Normalization 3

---
+++ normalization3.sql
@@ -0,0 +1,44 @@
+﻿DROP TABLE IF EXISTS staff;
+
+
+
+-- Staff --
+CREATE TABLE staff (
+  staffID     	char(8) not null,
+  fname    	text,
+  lname    	text,
+  age 		integer,
+ primary key(staffID)
+);        
+
+
+--Engineer -- 
+CREATE TABLE engineers (
+  staffID 	char(8) not null,
+  degree	text,
+  videogame	text,
+  primary key (staffID)
+  );
+
+-- Flight Control Operators -- 
+CREATE TABLE flightControlOperators (
+  staffID	char(8) not null,
+  chair		text,
+  drink		text,
+  hangover	text,
+  primary key (staffID)
+  );
+
+
+-- Astronauts --
+CREATE TABLE astronauts (
+  staffID 	char(8) not null,
+  yearsFlying	integer,
+  handicap	integer,
+  spouse	text,
+  primary key (staffID)
+  );
+
+-- Spacecrafts --
+CREATE TABLE spacecrafts (
+  sid, 
