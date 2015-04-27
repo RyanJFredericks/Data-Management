17a1b7a070fc03aeaf7494ad24a88085ee84c472
RyanJFredericks <ryan.fredericks1@marist.edu>
2015-04-27 11:25:16 -0400

    Stored Procedures

---
+++ storedproc.sql
@@ -0,0 +1,48 @@
+﻿--Ryan Fredericks
+--4/27/15
+--
+-- An example stored procedure ("function")
+--
+CREATE LANGUAGE plpgsql;
+
+--PreReqsFor stored procedure
+create or replace function PreReqsFor(integer, REFCURSOR) returns refcursor as 
+$$
+declare
+   num int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+      select preReqNum
+      from   Prerequisites
+       where  num = courseNum;
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select PreReqsFor(499, 'results');
+Fetch all from results;
+
+
+
+--IsPreReqFor stored procedure
+create or replace function IsPreReqFor(integer, REFCURSOR) returns refcursor as 
+$$
+declare
+   num int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+      select courseNum
+      from   Prerequisites
+       where  num = preReqNum;
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select IsPreReqFor(120, 'results');
+Fetch all from results;
+
+
